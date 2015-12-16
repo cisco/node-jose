2a05a6700b5828a32d5b51e707b4c171a08d3ec4
Matthew A. Miller <linuxwolf@outer-planes.net>
2015-12-16 15:07:21 -0700

    Update: support 'crit' header member

---
+++ test/jwe/crit-test.js
@@ -0,0 +1,131 @@
+"use strict";
+
+var chai = require("chai");
+
+var JWE = require("../../lib/jwe"),
+    JWK = require("../../lib/jwk");
+
+var assert = chai.assert;
+
+describe("jwe/crit", function() {
+  var key = {
+    "kty": "oct",
+    "kid": "jws-crit-test",
+    "k": "weCXXPzQGigKoxePr7gk9vGtPVHec_453r9vajS4dmM",
+    "alg": "A256GCM"
+  };
+
+  before(function() {
+    return JWK.asKey(key).
+          then(function(result) {
+            key = result;
+          });
+  });
+  it("by default fails decrypt if 'crit' is specified", function() {
+    var opts = {
+      fields: {
+        crit: ["exp"],
+        exp: Date.now()
+      }
+    };
+    var jwe = JWE.createEncrypt(opts, key);
+    jwe.update(new Buffer("something that should fail"));
+    var p = jwe.final();
+    p = p.then(function(jwe) {
+      return JWE.createDecrypt(key).
+             decrypt(jwe).
+             then(function() {
+               assert.ok(false, "unexpected success");
+             }).
+             catch(function(err) {
+               assert.ok(err instanceof Error);
+             });
+    });
+    return p;
+  });
+  it("succeeds if opts.handlers has a boolean property for 'crit'", function() {
+    var opts = {
+      fields: {
+        crit: ["exp"],
+        exp: Date.now()
+      },
+      handlers: {
+        exp: true
+      }
+    };
+    var jwe = JWE.createEncrypt(opts, key);
+    jwe.update(new Buffer("something that should not fail"));
+    var p = jwe.final();
+    p = p.then(function(jwe) {
+      return JWE.createDecrypt(key).
+             decrypt(jwe, opts).
+             then(function(result) {
+               assert.ok(result);
+             });
+    });
+    return p;
+  });
+  it("succeeds if opts.handlers has a function property for 'crit'", function() {
+    var opts;
+    opts = {
+      fields: {
+        crit: ["exp"],
+        exp: Date.now()
+      },
+      handlers: {
+        exp: function(jwe) {
+          assert.equal(jwe.header.exp, opts.fields.exp);
+        }
+      }
+    };
+    var jwe = JWE.createEncrypt(opts, key);
+    jwe.update(new Buffer("something that should not fail"));
+    var p = jwe.final();
+    p = p.then(function(jwe) {
+      return JWE.createDecrypt(key).
+             decrypt(jwe, opts).
+             then(function(result) {
+               assert.ok(result);
+             });
+    });
+    return p;
+  });
+  it("succeeds if opts.handlers has an object property for 'crit'", function() {
+    var opts;
+    opts = {
+      fields: {
+        crit: ["exp"],
+        exp: 1450474323149
+      },
+      handlers: {
+        exp: {
+          prepare: function(jwe) {
+            assert.ok(JWK.isKey(jwe.key));
+            assert.equal(typeof jwe.header, "object");
+            assert.ok(Array.isArray(jwe.protected));
+            assert.equal(typeof jwe.iv, "string");
+            assert.equal(typeof jwe.tag, "string");
+            assert.equal(typeof jwe.ciphertext, "string");
+          },
+          complete: function(jwe) {
+            assert.ok(JWK.isKey(jwe.key));
+            assert.equal(typeof jwe.header, "object");
+            assert.ok(Array.isArray(jwe.protected));
+            assert.ok(Buffer.isBuffer(jwe.payload));
+          }
+        }
+      }
+    };
+    var jwe = JWE.createEncrypt(opts, key);
+    jwe.update(new Buffer("something that should not fail"));
+    var p = jwe.final();
+    p = p.then(function(jwe) {
+      return JWE.createDecrypt(key).
+             decrypt(jwe, opts).
+             then(function(result) {
+               assert.ok(result);
+             });
+    });
+    return p;
+  });
+});
---
+++ test/jws/crit-test.js
@@ -0,0 +1,135 @@
+"use strict";
+
+var chai = require("chai");
+
+var JWS = require("../../lib/jws"),
+    JWK = require("../../lib/jwk");
+
+var assert = chai.assert;
+
+describe("jws/crit", function() {
+  var key = {
+    "kty": "oct",
+    "kid": "jws-crit-test",
+    "k": "weCXXPzQGigKoxePr7gk9vGtPVHec_453r9vajS4dmM",
+    "alg": "HS256"
+  };
+
+  before(function() {
+    return JWK.asKey(key).
+          then(function(result) {
+            key = result;
+          });
+  });
+  it("by default fails verify if 'crit' is specified", function() {
+    var opts = {
+      fields: {
+        crit: ["exp"]
+      }
+    };
+    var jws = JWS.createSign(opts, key);
+    jws.update(new Buffer("something that should fail"));
+    var p = jws.final();
+    p = p.then(function(jws) {
+      return JWS.createVerify(key).
+             verify(jws).
+             then(function() {
+               assert.ok(false, "unexpected success");
+             }).
+             catch(function(err) {
+               assert.ok(err instanceof Error);
+             });
+    });
+    return p;
+  });
+  it("succeeds if opts.handlers has a boolean property for 'crit'", function() {
+    var opts = {
+      fields: {
+        crit: ["exp"],
+        exp: Date.now()
+      },
+      handlers: {
+        exp: true
+      }
+    };
+    var jws = JWS.createSign(opts, key);
+    jws.update(new Buffer("something that should not fail"));
+    var p = jws.final();
+    p = p.then(function(jws) {
+      return JWS.createVerify(key).
+             verify(jws, opts).
+             then(function(result) {
+               assert.ok(result);
+             });
+    });
+    return p;
+  });
+  it("succeeds if opts.handlers has a function property for 'crit'", function() {
+    var opts = {
+      fields: {
+        crit: ["exp"],
+        exp: Date.now()
+      },
+      handlers: {
+        exp: function(jws) {
+          assert.ok(JWK.isKey(jws.key));
+          assert.equal(typeof jws.header, "object");
+          assert.ok(Array.isArray(jws.protected));
+          assert.equal(typeof jws.payload, "string");
+          assert.ok(-1 !== jws.protected.indexOf("crit"));
+          assert.equal(jws.header.exp, opts.fields.exp);
+        }
+      }
+    };
+    var jws = JWS.createSign(opts, key);
+    jws.update(new Buffer("something that should not fail"));
+    var p = jws.final();
+    p = p.then(function(jws) {
+      return JWS.createVerify(key).
+             verify(jws, opts).
+             then(function(result) {
+               assert.ok(result);
+             });
+    });
+    return p;
+  });
+  it("succeeds if opts.handlers has an object property for 'crit'", function() {
+    var opts = {
+      fields: {
+        crit: ["exp"],
+        exp: Date.now()
+      },
+      handlers: {
+        exp: {
+          prepare: function(jws) {
+            assert.ok(JWK.isKey(jws.key));
+            assert.equal(typeof jws.header, "object");
+            assert.ok(Array.isArray(jws.protected));
+            assert.equal(typeof jws.payload, "string");
+            assert.ok(-1 !== jws.protected.indexOf("crit"));
+            assert.equal(jws.header.exp, opts.fields.exp);
+          },
+          complete: function(jws) {
+            assert.ok(JWK.isKey(jws.key));
+            assert.equal(typeof jws.header, "object");
+            assert.ok(Array.isArray(jws.protected));
+            assert.ok(Buffer.isBuffer(jws.payload));
+            assert.ok(-1 !== jws.protected.indexOf("crit"));
+            assert.equal(jws.header.exp, opts.fields.exp);
+          }
+        }
+      }
+    };
+    var jws = JWS.createSign(opts, key);
+    jws.update(new Buffer("something that should not fail"));
+    var p = jws.final();
+    p = p.then(function(jws) {
+      return JWS.createVerify(key).
+             verify(jws, opts).
+             then(function(result) {
+               assert.ok(result);
+             });
+    });
+    return p;
+  });
+});
