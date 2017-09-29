0a6e324eb5d163d69a58c5cf592cde84057faa40
Matthew A. Miller <linuxwolf@outer-planes.net>
2017-09-29 11:31:21 -0600

    Update: Provide PBKDF2-based algorithms publically (#139)

--- test/algorithms/pbes2-test.js
+++ test/algorithms/pbes2-test.js
@@ -11,7 +11,63 @@ var algorithms = require("../../lib/algorithms/"),
     util = require("../../lib/util");
 
 describe("algorithms/pbes2", function() {
-  var vectors = [
+  var deriveVectors = [
+    {
+      alg: "PBKDF2-SHA-256",
+      desc: "Password-based Key Derivation using HMAC-SHA-256 {password=password, salt=salt, iterations=1}",
+      password: new Buffer("password"),
+      salt: new Buffer("salt"),
+      iterations: 1,
+      length: 32,
+      derived: "120fb6cffcf8b32c43e7225256c4f837a86548c92ccc35480805987cb70be17b"
+    },
+    {
+      alg: "PBKDF2-SHA-256",
+      desc: "Password-based Key Derivation using HMAC-SHA-256 {password=password, salt=salt, iterations=2}",
+      password: new Buffer("password"),
+      salt: new Buffer("salt"),
+      iterations: 2,
+      length: 32,
+      derived: "ae4d0c95af6b46d32d0adff928f06dd02a303f8ef3c251dfd6e2d85a95474c43"
+    },
+    {
+      alg: "PBKDF2-SHA-256",
+      desc: "Password-based Key Derivation using HMAC-SHA-256 {password=password, salt=salt, iterations=4096}",
+      password: new Buffer("password"),
+      salt: new Buffer("salt"),
+      iterations: 4096,
+      length: 32,
+      derived: "c5e478d59288c841aa530db6845c4c8d962893a001ce4e11a4963873aa98134a"
+    },
+    {
+      alg: "PBKDF2-SHA-256",
+      desc: "Password-based Key Derivation using HMAC-SHA-256 {password=passwordPASSWORDpassword, salt=saltSALTsaltSALTsaltSALTsaltSALTsalt, iterations=4096}",
+      password: new Buffer("passwordPASSWORDpassword"),
+      salt: new Buffer("saltSALTsaltSALTsaltSALTsaltSALTsalt"),
+      iterations: 4096,
+      length: 40,
+      derived: "348c89dbcbd32b2f32d814b8116e84cf2b17347ebc1800181c4e2a1fb8dd53e1c635518c7dac47e9"
+    }
+  ];
+  deriveVectors.forEach(function(v) {
+    var key = v.password,
+        props = {
+          salt: v.salt,
+          iterations: v.iterations,
+          length: v.length
+        },
+        derived = v.derived;
+
+    it("performs " + v.alg + " (" + v.desc + ")", function() {
+      var promise = algorithms.derive(v.alg, key, props);
+      promise = promise.then(function(result) {
+        assert.equal(result.toString("hex"), derived);
+      });
+      return promise;
+    });
+  });
+
+  var encVectors = [
     {
       alg: "PBES2-HS256+A128KW",
       desc: "Password-Based Encryption using HMAC-SHA-256 and AES-128-KW",
@@ -43,7 +99,7 @@ describe("algorithms/pbes2", function() {
     }
   ];
 
-  vectors.forEach(function(v) {
+  encVectors.forEach(function(v) {
     var key = v.password,
         props = {
           p2s: v.salt,
